36a39e7ef2a6b8d3582457693fabf7566d9d6cd2
youchuangao <youchuangao@126.com>
2016-09-08 16:40:07 +0800

    helloworldtest

---
+++ README.md
@@ -0,0 +1,18 @@
+# vuetest1_ycg
+
+> first project of vue
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>PHP is the best language of the world!</h2>
+    <input type="text" v-model = "message">
+    <span v-text = "message"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!',
+      message:''
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
